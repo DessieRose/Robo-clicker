430565064993bf6639920267a04f3318b1f22ccd
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2026-06-24 18:03:17 +0200

    Add logo to StartScreen and include assets
    Render a new logo on the start screen by importing logo.png in StartScreen.jsx and adding a .start-screen__logo CSS class to position it above the background image. Also add two image assets (logo.png and start-image_2.png) to src/Images/start-page. Start button and existing start page image behavior are unchanged.

--- src/Components/StartScreen.jsx
+++ src/Components/StartScreen.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import startPageImg from '../Images/start-page/startpage.png';
+import logo from '../Images/start-page/logo.png';
 import './StartScreen.css';
 
 export default function StartScreen({ onStart }) {
   return (
     <div className="start-screen">
+      <img src={logo} alt="Robo Clicker Logo" className="start-screen__logo" />
       <img src={startPageImg} alt="Robo Clicker" className="start-screen__image" />
       <button className="start-screen__button" onClick={onStart}>
         START GAME
